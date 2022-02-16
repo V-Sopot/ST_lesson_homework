0a5e9226a9ad832cbb697551688f57e9050d92b7
v.sopot <v.sopot@systtech.ru>
2022-02-16 15:20:07 +0200

    Random game

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ("Hello, SYSTECH!")
+import random
+number = random.randrange(0, 50)
+z = -1
+while z !=number:
+    z = int(input('Введи число от 0 до 50'))
+    if z > number:
+        print('Загаданное число меньше')
+    elif z < number:
+        print('Загаданное число больше')
+    elif z ==number:
+        print('Ты угадал число!')
